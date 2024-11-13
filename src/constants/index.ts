f815f5cc84031b5e74a6eb90c039bc941dd52ed7
Alaa Shalaby <alaashalabi011@gmail.com>
2024-11-13 23:09:29 +0200

    Add nanlinks constant

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+export const navbarList = [
+  {
+    id: 1,
+    name: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Stays",
+    url: "/stays",
+  },
+  {
+    id: 3,
+    name: "Become a host",
+    url: "/book",
+  },
+];
