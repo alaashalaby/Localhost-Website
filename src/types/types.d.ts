94ead3ad01bdae93e9ba88005e2ba137edeec310
Alaa Shalaby <alaashalabi011@gmail.com>
2024-11-14 16:43:00 +0200

    Add types of Place

---
+++ src/types/types.d.ts
@@ -0,0 +1,11 @@
+interface Place {
+  createdAt: number;
+  name: string;
+  cover: string;
+  rate: number;
+  city: string;
+  country: string;
+  state: string;
+  price: number;
+  id: string;
+}
