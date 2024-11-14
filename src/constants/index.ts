a75181a2a61678c388fd05d96f0bd437b386a162
Alaa Shalaby <alaashalabi011@gmail.com>
2024-11-14 12:57:18 +0200

    Add footerData constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,3 +15,54 @@ export const navbarList = [
     url: "/book",
   },
 ];
+export const footerData = [
+  {
+    id: 10,
+    title: "Support",
+    listItems: [
+      {
+        id: 100,
+        listData: [
+          "Help Centre",
+          "AirCover",
+          "Combating discrimination",
+          "Supporting people with disabilities",
+          "Cencellation options",
+          "Report neighbourhood concern",
+        ],
+      },
+    ],
+  },
+  {
+    id: 11,
+    title: "Hosting",
+    listItems: [
+      {
+        id: 110,
+        listData: [
+          "Local home",
+          "Cover for hosts",
+          "Hosting resources",
+          "Community forum",
+          "Hosting responsibly",
+        ],
+      },
+    ],
+  },
+  {
+    id: 12,
+    title: "Localhost",
+    listItems: [
+      {
+        id: 120,
+        listData: [
+          "Newsroom",
+          "New features",
+          "Careers",
+          "Investors",
+          "Gift cards",
+        ],
+      },
+    ],
+  },
+];
